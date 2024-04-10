2419c9c7b54df29defc93cf471e89e39d770acda
aakash_rathor <154987650+aakashrathor2019@users.noreply.github.com>
2024-04-10 22:18:35 +0530

    Update fibonacci_series.py

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,17 +1,18 @@
-def get_fib_series(n):
-    print('value of n is--->>',n)
-    x=0
-    y=1
-    z= 0
-    while(n>=z):
-        print('inside the while loop')
-        x=y
-        y=z
-        z=x+y
-    return z
-    
+#Fibonacci Series Generate with mathematical recurrence 
+def fibonacci_generator():
+    a, b = 0, 1
+    yield a
+    yield b
+    while True:
+        if a <= b:
+            a = a + b
+            yield a
+        else:
+            b = a + b
+            yield b
 
-n=int(input("Enter the value of n:"))
-print('value of n  is--->',n)
-value=get_fib_series(n)
-print('Final value of finbonacci series is-->',value)
+print("Enter the value of n:")
+n = int(input())  # Convert input to integer
+fib_gen = fibonacci_generator()
+for i in range(n):
+    print(next(fib_gen))
